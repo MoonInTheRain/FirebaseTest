0175774faf4b57d411145ed34f545c00d88787d3
uesugi <wuesugi@hotmail.com>
2025-07-24 14:54:32 +0900

    [update] プッシュ通知の機構を追加。

---
+++ FirebaseTools/public/firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
+
+// Firebaseの初期化（Webアプリと同じconfig）
+firebase.initializeApp({
+  apiKey: "AIzaSyAXej2b2FrBVZqOKjUjjERGM2XioQuPEM0",
+  projectId: "litegame-df1fe",
+  messagingSenderId: "213956204660",
+  appId: "1:213956204660:web:f09be0cacbd2566f6f1921"
+});
+
+const messaging = firebase.messaging();
+
+// バックグラウンド通知を受け取ったときの処理
+messaging.onBackgroundMessage(payload => {
+  console.log('[Service Worker] 通知を受信:', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon.png', // 任意のアイコン
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
